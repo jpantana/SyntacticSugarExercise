d8feac3e306a9927dcb5a22183edec0dceebced1
jpantana <josh.pantana@gmail.com>
2019-09-04 16:44:26 -0500

    first commit

---
+++ SyntacticSugarExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyntacticSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
